1499ff47c6db6ade24b18ed559a88b7c630c29f9
Devipriya S <sdevipriya1234@gmail.com>
2026-08-22 02:26:23 +0530

    Update API key retrieval method in recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 load_dotenv()
 
-genai.configure(api_key=os.st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
